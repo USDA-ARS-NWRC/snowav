bcd738efa313d6f6b9d425da5ec5c15c30f67a47
Mark Robertson <markrobertson@u.boisestate.edu>
2018-04-20 10:24:53 -0600

    initial

---
+++ setup.py
@@ -0,0 +1,58 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    # TODO: put package requirements here
+]
+
+setup_requirements = [
+    # TODO(micahsandusky5): put setup requirements (distutils extensions, etc.) here
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='snowav',
+    version='0.1.0',
+    description="Snow and Water Model Analysis and Visualization ",
+    long_description=readme + '\n\n' + history,
+    author="Mark Robertson",
+    author_email='mark.robertson@ars.usda.gov',
+    url='https://github.com/roberton-mark/SNOWAV',
+    packages=['snowav',
+			  'snowav.methods',
+			  'snowav.report',
+			  'snowav.plotting'
+			  ],
+
+
+    include_package_data=True,
+    package_data={'snowav':['./config/CoreConfig.ini']},
+    install_requires=requirements,
+    license="GPL-3.0",
+    zip_safe=False,
+    keywords='snowav',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU GPL-3.0',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)
