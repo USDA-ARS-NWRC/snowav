61b2ed438d9745009026180d87969f4571a31ba1
Mark Robertson <markrobertson@u.boisestate.edu>
2018-04-27 15:09:23 -0600

    test

--- scripts/snow.py
+++ scripts/snow.py
@@ -5,7 +5,7 @@ import snowav
 
 def run(config_file):
 
-    # config_file = '/home/markrobertson/mrworkspace/code/SNOWAV/snowav/config/snowav_brb_wy2018.ini'
+    # config_file = '/home/markrobertson/mrworkspace/code/SNOWAV/snowav/config/snowav_brb_wy2018.ini '
     # config_file = '/mnt/volumes/wkspace/config/snowav/snowav_brb_wy2018_devel.txt'
     snow = snowav.plotting.framework.SNOWAV(config_file)
 
