bc5c3e52e624c6ba015b05aa9e1157724e6fa258
Mark Robertson <markrobertson@u.boisestate.edu>
2018-05-16 15:42:55 +0000

    added preprocess module for config/awsm functionality

--- scripts/snow.py
+++ scripts/snow.py
@@ -9,6 +9,7 @@ def run(config_file):
     # config_file = '/mnt/volumes/wkspace/config/snowav/snowav_brb_wy2018_devel.txt'
     # config_file = '/mnt/volumes/wkspace/config/snowav/snowav_tuol_wy2018.txt'
     snow = snowav.plotting.framework.SNOWAV(config_file = config_file)
+    snow.preprocess()
 
     if not hasattr(snow,'error'):
 
