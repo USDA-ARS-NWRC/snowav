8b8a476ec6393a8e6ca7020897087222fcd2505f
Mark Robertson <markrobertson@u.boisestate.edu>
2018-07-16 09:36:48 -0600

    scripts/create_database.py now functions as one-time database creation, with table classes available outside in tables

--- scripts/create_database.py
+++ scripts/create_database.py
@@ -1,6 +1,7 @@
 
 import sys
 import os
+import snowav
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -8,47 +9,41 @@ from sqlalchemy import create_engine
 from sqlalchemy import schema, types
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import backref
+from snowav.database.tables import Base, Basin_Metadata, Results
 
-# db_location = '/home/markrobertson/mrworkspace/projects/snowav_database.db'
+'''
+Currently this isn't working with relative 'db_location'...
+
+'''
+
 
 def run(db_location):
 
     if not os.path.isfile(db_location):
 
-        Base = declarative_base()
-
-        class Basin_Metadata(Base):
-            __tablename__ = 'Basin_Metadata'
-
-            basin_id = Column(Integer, primary_key=True, autoincrement=True)
-            basin_name = Column(String(250), nullable=False, unique=True)
-            state = Column(String(250), nullable=True)
-            area = Column(types.Float(), nullable=False)
-
-        class Results(Base):
-            __tablename__ = 'Results'
-
-            id = Column(Integer, primary_key=True)
-            basin_id = Column(Integer, ForeignKey('Basin_Metadata.basin_id'))
-            date_time = Column(types.DateTime(),nullable=False)
-            proc_time = Column(types.DateTime(),nullable=True)
-            version = Column(String(250), nullable=True)
-            variable = Column(String(250), nullable=False)
-            var_units = Column(String(250), nullable=False)
-            value = Column(types.Float(), nullable=False)
-            elevation = Column(String(250), nullable=False)
-            elev_units = Column(String(250), nullable=False)
-
-            # This puts Basin_Metadata.results and Results.basin_metadata
-            basin_metadata = relationship('Basin_Metadata',
-                                        backref=backref('results',lazy='dynamic'))
+        # Base = declarative_base()
+        # Basin_Metadata(Base)
+        # Results(Base)
 
         # Create an engine that stores data in the local directory's
         # sqlalchemy_example.db file.
-        engine = create_engine('sqlite:///%s'%(db_location))
+        engine = create_engine('sqlite:////home/markrobertson/mrworkspace/projects/snowavdb/snowav_db_test.db')
 
         # Create all tables in the engine.
         Base.metadata.create_all(engine)
+
+        # After db creation...
+        # DeclarativeBase = declarative_base()
+        # metadata = DeclarativeBase.metadata
+
+        # Bind the engine to the metadata of the Base class so that the
+        # declaratives can be accessed through a DBSession instance
+        # Base.metadata.bind = engine
+        # DBSession = sessionmaker(bind=engine)
+        # session = DBSession()
+        # session.commit()
+        # session.close()
+
 
     else:
 
