accc56be0cbdf14bb1dfb7e71228f26f83859b2a
Mark Robertson <markrobertson@u.boisestate.edu>
2018-07-16 13:38:21 -0600

    insert to database framework

--- snowav/database/database.py
+++ snowav/database/database.py
@@ -1,32 +1,53 @@
-import numpy as np
+
 import os
-import sys
 import datetime
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
-from sqlalchemy import schema, types
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import backref
+from snowav.database.tables import Basin_Metadata, Base, Results, Run_Metadata
 
-def update():
-    # After db creation...
+# Coming from scripts/snow.py
+# values = {}
+# snow.db_variables
+
+def insert_results(snow,values):
+    '''
+    values = {'basin_id': 1,
+              'date_time': datetime.datetime.now(),
+              'proc_time': datetime.datetime.now(),
+              'version': '1',
+              'variable': 'swe',
+              'var_units': 'in',
+              'value': 2.0,
+              'elevation': 'total',
+              'elev_units': 'ft'}
+
+    '''
+
+    loc = snow.database
+
+    engine = create_engine('sqlite:///%s'%(loc))
     DeclarativeBase = declarative_base()
     metadata = DeclarativeBase.metadata
+    Base.metadata.bind = engine
 
     # Bind the engine to the metadata of the Base class so that the
     # declaratives can be accessed through a DBSession instance
-    Base.metadata.bind = engine
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
 
-    basin_names = ['Boise River Basin','Featherville']
-    areas = np.array((100,30))
+    val = Results(basin_id = values['basin_id'],
+                  date_time = values['date_time'],
+                  proc_time = values['proc_time'],
+                  version = values['version'],
+                  variable = values['variable'],
+                  var_units = values['var_units'],
+                  value = values['value'],
+                  elevation = values['elevation'],
+                  elev_units = values['elev_units'] )
 
-    for bn,a in zip(basin_names,areas):
-        brb = Basin_Metadata(basin_name = bn, state = 'Idaho', area = a)
-        session.add(brb)
-
+    session.add(val)
     session.commit()
     session.close()
