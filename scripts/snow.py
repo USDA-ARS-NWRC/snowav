c256e66a26ce3f387326339619d47055e155fc65
Mark Robertson <markrobertson@u.boisestate.edu>
2018-08-06 19:07:11 +0000

    update notes

--- scripts/snow.py
+++ scripts/snow.py
@@ -3,6 +3,12 @@ import sys
 import snowav
 
 '''
+- basin_total() needs cleaned up; pull from database once all runs available;
+    some dataframes are built on the order they are pulled from db rather than
+    chronological order
+- output csv option
+- SWI versus streamflow (-> streamflow on db?)
+- SWE v distance to reservoir?
 
 '''
 
