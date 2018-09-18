82ec48fff3bc9270e98499c4f578adc6db24cf2f
Mark Robertson <markrobertson@u.boisestate.edu>
2018-09-18 20:34:46 +0000

    add actual arg parsing

--- scripts/database_overview.py
+++ scripts/database_overview.py
@@ -20,28 +20,33 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 import copy
 import matplotlib.dates as mdates
+import argparse
 
 '''
 List available runs on database by basin and water year.
 
-python scripts/database_overview.py 'Basin' wy
+$ python scripts/database_overview.py 'Basin River Basin' 2017
 
 '''
 
 if __name__ == '__main__':
-    bid = sys.argv[1]
 
-    if len(sys.argv) > 2:
-        wy = int(sys.argv[2])
-    else:
-        wy = 2018
+    parser = argparse.ArgumentParser(description="Examine and auto populate")
+
+    parser.add_argument('-b', '--bid', dest='bid', type=str,
+                        help='Basin id, from snowav/database/tables.py')
+
+    parser.add_argument('-wy', '--wy', dest='wy', type=int,
+                        help='Water year')
+
+    args = parser.parse_args()
 
 # Making these defaults for now
 location = 'sqlite:////home/markrobertson/wkspace/projects/snowavdb/snowavdb.db'
 value = 'swe_vol'
 
-start_date = datetime.datetime(wy - 1,10,1)
-end_date = datetime.datetime(wy,9,30)
+start_date = datetime.datetime(args.wy - 1,10,1)
+end_date = datetime.datetime(args.wy,9,30)
 
 # Connect to database and query
 engine = create_engine(location)
@@ -50,11 +55,11 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 qry = session.query(Results).filter(and_((Results.date_time >= start_date),
                                           (Results.date_time <= end_date),
-                                          (Results.basin_id == BASINS.basins[bid]['basin_id'])))
+                                          (Results.basin_id == BASINS.basins[args.bid]['basin_id'])))
 
 df = pd.read_sql(qry.statement, qry.session.connection())
 
 session.close()
 
 print('On database: {},\nAvailable runs for {}: {},\nDate range: {}, {}'.format(
-location,bid,df.run_name.unique(),df['date_time'].min(),df['date_time'].max()))
+location,args.bid,df.run_name.unique(),df['date_time'].min(),df['date_time'].max()))
