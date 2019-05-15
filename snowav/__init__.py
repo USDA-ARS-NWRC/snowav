201503e65e67ec6e0aceb283695362e61ed68e0a
Mark Robertson <markrobertson@u.boisestate.edu>
2019-05-15 14:26:48 +0000

    Bump version: 0.8.1 → 0.8.2

--- snowav/__init__.py
+++ snowav/__init__.py
@@ -1,5 +1,5 @@
 import os
-__version__ = '0.8.1'
+__version__ = '0.8.2'
 __core_config__ = os.path.abspath(os.path.dirname(__file__) + '/config/CoreConfig.ini')
 __recipes__ = os.path.abspath(os.path.dirname(__file__) + '/config/recipes.ini')
 
