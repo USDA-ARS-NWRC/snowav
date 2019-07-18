c73162f00e8f2cc824e0d7ca63e504e25300625f
Mark Robertson <markrobertson@u.boisestate.edu>
2019-07-18 14:58:00 +0000

    Bump version: 0.9.0 → 0.9.1

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ test_requirements = []
 
 setup(
     name='snowav',
-    version='0.9.0',
+    version='0.9.1',
     description="Snow and Water Model Analysis and Visualization ",
     author="Mark Robertson",
     author_email='mark.robertson@ars.usda.gov',
