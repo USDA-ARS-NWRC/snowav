f6fd03c54ebdb90dcd62134c24ba3a6cb0a4448b
Mark Robertson <markrobertson@u.boisestate.edu>
2019-08-02 14:52:01 +0000

    Remove unused and outdated figures

--- snowav/plotting/__init__.py
+++ snowav/plotting/__init__.py
@@ -6,16 +6,12 @@ from . import density
 from . import image_change
 from . import flt_image_change
 from . import swe_change
-from . import pixel_swe
 from . import stn_validate
-from . import precip_validate
 from . import basin_detail
 from . import compare_runs
 from . import precip_depth
 from . import point_values
-from . import air_temp
 from . import cold_content
 from . import swe_volume
 from . import plotlims
-from . import subbasins
 from . import write_properties
