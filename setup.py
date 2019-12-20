17c506fe4cdfd8cab263fb760bb5d4ba9c419eab
Mark Robertson <markrobertson@u.boisestate.edu>
2019-12-20 20:24:45 +0000

    Bump version: 0.10.6 → 0.10.7

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ test_requirements = []
 
 setup(
     name='snowav',
-    version='0.10.6',
+    version='0.10.7',
     description="Snow and Water Model Analysis and Visualization ",
     author="Mark Robertson",
     author_email='mark.robertson@ars.usda.gov',
--- snowav/__init__.py
+++ snowav/__init__.py
@@ -1,5 +1,5 @@
 import os
-__version__ = '0.10.6'
+__version__ = '0.10.7'
 __core_config__ = os.path.abspath(os.path.dirname(__file__) + '/config/CoreConfig.ini')
 __recipes__ = os.path.abspath(os.path.dirname(__file__) + '/config/recipes.ini')
 __config_titles__ = {'snowav':'Overview',
