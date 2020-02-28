4f143ca76da5274a0d5ba8224129a48064eae907
Mark Robertson <markrobertson@u.boisestate.edu>
2020-02-28 21:42:29 +0000

    Consolidate reading and parsing config options into UserConfig class

--- snowav/framework/framework.py
+++ snowav/framework/framework.py
@@ -1,21 +1,20 @@
 
+from datetime import datetime
 import os
+
 from snowav.framework.query import query
-from snowav.framework.read_config import read_config
+from snowav.framework.config import UserConfig
 from snowav.framework.process import process
-from snowav.framework.parse import parse
 from snowav.framework.figures import figures
 from snowav.report.report import report
 from snowav.database.database import run_metadata
 from snowav.inflow.inflow import excel_to_csv
-from datetime import datetime
 
-class snowav(object):
+class Snowav(object):
 
     def __init__(self, config_file = None, external_logger = None, awsm = None,
                  end_date = None):
-        '''
-        Read config file, parse config options, process results, put results
+        """ Read config file, parse config options, process results, put results
         on database, make figures, and make pdf report.
 
         Args
@@ -26,91 +25,75 @@ class snowav(object):
             awsm logger
         awsm : class
             awsm class if being run in awsm
+        """
 
-        '''
-
-        # Get config options
-        if awsm is None and os.path.isfile(config_file):
-            self.config_file = config_file
-            read_config(self, end_date = end_date)
-
-        elif awsm is not None:
-            self.config_file = awsm.configFile
-            read_config(self, awsm = awsm, end_date = end_date)
-
-        else:
-            raise Exception('No config instance passed, or config file does '
-                            'not exist!')
+        # get and parse config options
+        cfg = UserConfig(config_file, awsm = awsm, end_date = end_date)
+        cfg.parse()
 
         # query existing database without processing
-        if self.query_flag:
-            query(self)
-
-        # parse config options
-        Variables = parse(self)
+        if cfg.query_flag:
+            query(cfg)
 
         # put run metadata on database
-        run_metadata(self, Variables, self.run_name)
+        run_metadata(cfg, cfg.variables, cfg.run_name)
 
         # process results
-        self.pargs['run_id'] = self.run_id
-        self.pargs['vid'] = self.vid
-        self.precip_flag, out, pre, rain, density = process(self.pargs)
+        cfg.pargs['run_id'] = cfg.run_id
+        cfg.pargs['vid'] = cfg.vid
+        cfg.precip_flag, out, pre, rain, density = process(cfg.pargs)
 
-        # gather process() outputs
-        # for log in out:
-        #     self._logger.info(log)
+        # assign
+        cfg.density = density
+        cfg.rain_total = rain
+        cfg.precip_total = pre
 
-        self.density = density
-        self.rain_total = rain
-        self.precip_total = pre
+        if not cfg.precip_flag:
+            cfg.precip_depth_flag = False
+            cfg.precip_validate_flag = False
 
-        if not self.precip_flag:
-            self.precip_depth_flag = False
-            self.precip_validate_flag = False
-
-        if self.inflow_flag and self.inflow_data is not None:
+        if cfg.inflow_flag and cfg.inflow_data is not None:
             args = {}
-            args['path'] = self.inflow_data
-            args['csv_file'] = self.summary_csv
-            args['basin_headings'] = self.basin_headings
-            args['inflow_headings'] = self.inflow_headings
-            args['file_base'] = self.file_base
-            args['sheet_name'] = self.sheet_name
-            args['skiprows'] = self.skiprows
-            args['date_idx'] = self.date_idx
-            args['wy'] = self.wy
-            args['overwrite'] = self.overwrite
-            args['convert'] = self.convert
+            args['path'] = cfg.inflow_data
+            args['csv_file'] = cfg.summary_csv
+            args['basin_headings'] = cfg.basin_headings
+            args['inflow_headings'] = cfg.inflow_headings
+            args['file_base'] = cfg.file_base
+            args['sheet_name'] = cfg.sheet_name
+            args['skiprows'] = cfg.skiprows
+            args['date_idx'] = cfg.date_idx
+            args['wy'] = cfg.wy
+            args['overwrite'] = cfg.overwrite
+            args['convert'] = cfg.convert
 
-            excel_to_csv(args, self._logger)
+            excel_to_csv(args, cfg._logger)
 
-        figures(self)
+        figures(cfg)
 
         # Do additional processing and figures if forecast is supplied. Some
         # field will be overwritten during forecast processing
-        if self.forecast_flag:
-            self._logger.info(' Starting forecast processing...')
+        if cfg.forecast_flag:
+            cfg._logger.info(' Starting forecast processing...')
 
-            run_metadata(self, self.for_run_name)
+            run_metadata(cfg, cfg.for_run_name)
 
-            self.pargs['run_id'] = self.run_id
-            self.pargs['vid'] = self.vid
-            flags, out, pre, rain, density = process(self.pargs)
+            cfg.pargs['run_id'] = cfg.run_id
+            cfg.pargs['vid'] = cfg.vid
+            flags, out, pre, rain, density = process(cfg.pargs)
 
             for log in out:
-                self._logger.info(log)
+                cfg._logger.info(log)
 
-            self.density = density
-            self.rain_total = rain
-            self.precip_total = pre
+            cfg.density = density
+            cfg.rain_total = rain
+            cfg.precip_total = pre
 
             # figures for forecast run
-            figures(self)
+            figures(cfg)
 
-        if self.report_flag:
-            report(self)
+        if cfg.report_flag:
+            report(cfg)
 
-        elapsed = str(datetime.now() - self.proc_time_start)
+        elapsed = str(datetime.now() - cfg.proc_time_start)
 
-        self._logger.info(' Completed snowav processing, elapsed time: {}'.format(elapsed))
+        cfg._logger.info(' Completed snowav processing, elapsed time: {}'.format(elapsed))
